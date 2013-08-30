bf6df60f56fe5710142fe0401a18f14a422f1298
Ian G <yo.ian.g@gmail.com>
2013-08-30 10:54:36 -0700

    example project to test with

---
+++ Example/XibExporterExample/XibExporterExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XibExporterExample
+//
+//  Created by Ian on 8/30/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XibExporterExample/XibExporterExampleTests/XibExporterExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  XibExporterExampleTests.m
+//  XibExporterExampleTests
+//
+//  Created by Ian on 8/30/13.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XibExporterExampleTests : XCTestCase
+
+@end
+
+@implementation XibExporterExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
